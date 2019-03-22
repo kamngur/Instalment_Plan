e94819114e09366ed5a87d413503277d528533ae
Kasia <795kasia@gmail.com>
2019-03-22 13:57:56 +0300

    transfer of methods createProgile & createCustomer to class Customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Customer {
     private static int nextId = 1;
     private int id = assignId();
@@ -32,4 +34,36 @@ public class Customer {
         return (this.id + " " + this.name + " " + this.surname);
     }
 
+    static int createProfile() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Chcesz stworzyć profil?" + "\n" + "Wybierz:" + "\n" + "1. Utwórz profil." + "\n" + "2. Nie jestem zainteresowany/a."+ "\n"+"3. Przejdź dalej.");
+        int answer = 0;
+        do {
+            answer = scanner.nextInt();
+            switch (answer) {
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("Szkoda, że nie jesteś zainteresowany.");
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Nie wybrałeś, żadnego wariantu. Sprubój ponownie.");
+                    break;
+            }
+        }
+        while (answer != 1 && answer != 2 && answer != 3);
+        return answer;
+    }
+
+    static Customer createCustomer() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imie:");
+        String name = scanner.nextLine();
+        System.out.println("Podaj nazwisko:");
+        String surname = scanner.nextLine();
+        Customer customer = new Customer(name, surname);
+        return customer;
+    }
 }
